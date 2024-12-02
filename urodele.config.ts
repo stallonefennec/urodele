c38cd6cc8dba88d9e18920f9482694f837f38c68
stallonefennec <43107490+stallonefennec@users.noreply.github.com>
2024-12-02 21:56:14 +0800

    Update urodele.config.ts

--- urodele.config.ts
+++ urodele.config.ts
@@ -1,7 +1,7 @@
 export const config = {
   github: {
-    login: "glink25", // github login name, not user name
-    repo: "test-for-anything", //"urodele",
+    login: "stallonefennec", // github login name, not user name
+    repo: "urodele", //"urodele",
     logInUrl: "",
     logInAuthUrl: "",
   },
